6e771290e1981ab66c5a1422dd56735889a9debd
anushkadeshpande <anushkaa5000@gmail.com>
2023-03-04 23:24:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+// import { Model } from './Nutella';
+import { Model } from './Nutella.jsx';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas color='0xE6EF47' >
+      
+        <ambientLight />
+        <pointLight position={[5,5 ,5 ]} intensity={1} />
+        <pointLight position={[-3, -3, 2]} />
+        <OrbitControls />
+      <Model />
+      </Canvas>
     </div>
   );
 }
